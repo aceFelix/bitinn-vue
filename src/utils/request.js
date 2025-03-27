2907282b2795504e9d806669465955e93c6a1629
ACE-Felix <13985465782@163.com>
2025-03-27 16:36:01 +0800

    初始化项目并完成登录注册功能

---
+++ src/utils/request.js
@@ -0,0 +1,31 @@
+// 定制请求的实例
+ 
+// 导入axios  npm install axios
+import axios from 'axios'
+// 导入ElMassage组件
+import { ElMessage } from 'element-plus'
+// 定义一个变量,记录公共的前缀  ,  baseURL
+// const baseURL = 'http://localhost:8080';
+const baseURL = '/api';
+const instance = axios.create({baseURL})
+
+
+// 添加响应拦截器
+instance.interceptors.response.use(
+    // 判断业务状态码
+    res=>{
+        if(res.data.code==0){
+            return res.data;
+        }else{
+            // alert(res.data.msg?res.data.msg:'服务异常');
+            ElMessage.error(res.data.msg?res.data.msg:'服务异常');
+        }
+    },
+    err=>{
+        // alert('服务异常');
+        ElMessage.error('服务异常');
+        return Promise.reject(err);  // 异步的状态转化成失败的状态
+    }
+)
+
+export default instance;
--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': { // 代理地址，前端请求地址
+        target:'http://localhost:8080', // 代理目标地址，后端服务地址
+        changeOrigin:true, // 是否改变请求头
+        rewrite:path=>path.replace(/^\/api/,'') // 重写路径,将/api替换为空
+      }
+    }
+  }
 })
